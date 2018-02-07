3a989ed48d0072bdf2bfb20f5384d8c3572a2a24
Guy Aviv <guyaviv0@walla.com>
2018-02-07 21:51:35 +0200

    Add CategoryMembersClasses

---
+++ WikipediaActivity/CategoryMembersClasses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikipediaActivity.CategoryMembers
+{
+    public class Limits
+    {
+        public int categorymembers { get; set; }
+    }
+
+    public class Categorymember
+    {
+        public int ns { get; set; }
+        public string title { get; set; }
+    }
+
+    public class Query
+    {
+        public List<Categorymember> categorymembers { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string batchcomplete { get; set; }
+        public Limits limits { get; set; }
+        public Query query { get; set; }
+    }
+}
