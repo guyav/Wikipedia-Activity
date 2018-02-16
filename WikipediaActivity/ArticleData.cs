2d0a223acdae414a9b354e128ea397354b7524cd
Guy Aviv <guyaviv0@walla.com>
2018-02-16 22:59:58 +0200

    Change property names to start with capital letter, Add Score property

--- WikipediaActivity/ArticleData.cs
+++ WikipediaActivity/ArticleData.cs
@@ -8,26 +8,50 @@ namespace WikipediaActivity
 {
     class ArticleData
     {
-        public string name { get; private set; }
-        public int numberOfChanges { get; private set; }
-        public int changesDelta { get; private set; }
-        public int numberOfTalkChanges { get; private set; }
-        public int talkChangesDelta { get; private set; }
-        public bool inNotabilityCategory { get; set; }
-        public bool inDeletionCategory { get; set; }
+        //TODO: Add metric for the number of users who edited the article/talk page.
+        public string Name { get; private set; }
+        public int NumberOfChanges { get; private set; }
+        public int ChangesDelta { get; private set; }
+        public int NumberOfTalkChanges { get; private set; }
+        public int TalkChangesDelta { get; private set; }
+        public bool InNotabilityCategory { get; set; }
+        public bool InDeletionCategory { get; set; }
 
         public ArticleData(string name, int numberOfChanges, int changesDelta, int numberOfTalkChanges, int talkChangesDelta)
         {
-            this.name = name;
-            this.numberOfChanges = numberOfChanges;
-            this.changesDelta = changesDelta;
-            this.numberOfTalkChanges = numberOfTalkChanges;
-            this.talkChangesDelta = talkChangesDelta;
+            this.Name = name;
+            this.NumberOfChanges = numberOfChanges;
+            this.ChangesDelta = changesDelta;
+            this.NumberOfTalkChanges = numberOfTalkChanges;
+            this.TalkChangesDelta = talkChangesDelta;
         }
 
         public override string ToString()
         {
-            return this.name;
+            return this.Name;
+        }
+
+        public int Score
+        {
+            get
+            {
+                int score = 0;
+                const int CHANGE_COUNT_SCORE = 20;
+                const int CHANGE_DELTA_SCORE = 1;
+                const int TALK_COUNT_SCORE = 40;
+                const int TALK_DELTA_SCORE = 5;
+                const int NOTABILITY_SCORE = 100;
+                const int DELETION_SCORE = 100;
+
+                score += this.NumberOfChanges * CHANGE_COUNT_SCORE;
+                score += this.ChangesDelta * CHANGE_DELTA_SCORE;
+                score += this.NumberOfTalkChanges * TALK_COUNT_SCORE;
+                score += this.TalkChangesDelta * TALK_DELTA_SCORE;
+                score += Convert.ToInt32(InNotabilityCategory) * NOTABILITY_SCORE;
+                score += Convert.ToInt32(InDeletionCategory) * DELETION_SCORE;
+
+                return score;
+            }
         }
     }
 }
